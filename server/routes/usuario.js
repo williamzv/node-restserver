339d307642393224526207be17228ebf4f3eb3d0
William Zamora <williamzv@gmail.com>
2018-04-16 22:56:57 -0600

    Login normal, seccióon 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,10 +5,11 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 const app = express();
 
-app.get('/usuario', (req, res) => {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,7 +37,7 @@ app.get('/usuario', (req, res) => {
         });
 });
 
-app.post('/usuario', (req, res) => {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -60,7 +61,7 @@ app.post('/usuario', (req, res) => {
     });
 });
 
-app.put('/usuario/:id', (req, res) => {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']); //se indican que campos se pueden actualizar.
 
@@ -78,7 +79,7 @@ app.put('/usuario/:id', (req, res) => {
     });
 });
 
-app.delete('/usuario/:id', (req, res) => {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     let cambiaEstado = {
         estado: false
--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json());
-
-app.use(require('./routes/usuario.js'));
+// Configuración global de rutas
+app.use(require('./routes/index.js'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
